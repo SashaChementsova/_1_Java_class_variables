24bcec63a4408b2ecdf7a14b5164df6d32016d9f
Aliaksandr <skander.95@yandex.ru>
2024-07-24 15:25:38 +0300

    JFB-2

--- src/main/java/task1/TicketService.java
+++ src/main/java/task1/TicketService.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class TicketService
 {
-    private static List<Ticket> tickets;
+    private static List<Ticket> tickets = new ArrayList<>();
 
     public TicketService() {
         tickets = new ArrayList<>();
